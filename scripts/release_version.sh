4598d4610bdd3a9e4a257c5d3bd61bec9053f54a
Shelby Doolittle <shelby@shelbyd.com>
2015-02-02 19:39:37 -0800

    add release_version.sh script

---
+++ scripts/release_version.sh
@@ -0,0 +1,11 @@
+git co . &&
+git pull -r &&
+bundle exec rake &&
+sed -i "s/VERSION.*/VERSION = \"$1\"/g" ./lib/story_express/version.rb &&
+git ci -am "version $1" &&
+git tag $1 &&
+git push &&
+git push origin $1 &&
+rm *.gem &&
+gem build story_express.gemspec
+gem push "story_express-$1.gem"
